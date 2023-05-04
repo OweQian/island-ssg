b6555f0dd9a1e0a93fa4bdefeecf19da1f59d060
OweQian <1510106069@qq.com>
2023-05-04 17:18:48 +0800

    feat: 配置 Dev Server

--- dist/node/cli.js
+++ dist/node/cli.js
@@ -1,8 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const dev_1 = require("./dev");
+const path = require("path");
 const cli = require('cac')();
 const version = require('../../package.json').version;
 cli.version(version).help();
 cli.command('dev [root]', 'start dev server').action(async (root) => {
-    console.log('dev', root);
+    root = root ? path.resolve(root) : process.cwd();
+    const server = await (0, dev_1.createDevServer)(root);
+    await server.listen();
+    server.printUrls();
 });
 cli.command('build [root]', 'build for production').action(async (root) => {
     console.log('build', root);
--- src/node/cli.ts
+++ src/node/cli.ts
@@ -1,10 +1,16 @@
+import { createDevServer } from './dev';
+import * as path from 'path';
+
 const cli = require('cac')()
 const version = require('../../package.json').version;
 
 cli.version(version).help();
 
 cli.command('dev [root]', 'start dev server').action(async (root: string) => {
-  console.log('dev', root);
+  root = root ? path.resolve(root) : process.cwd();
+  const server = await createDevServer(root);
+  await server.listen();
+  server.printUrls();
 });
 
 cli.command('build [root]', 'build for production').action(async (root: string) => {
